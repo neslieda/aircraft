1fafead1e87e3fd0ee20637696a0f38e9542da86
Neslihan Eda Yürek <124497753+neslieda@users.noreply.github.com>
2025-02-04 14:36:26 +0300

    Delete airplane/frame-1

--- airplane/frame-1/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# frame > 2024-06-20 9:11am
-https://universe.roboflow.com/mia-pqqxd/frame-y9ksc
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- airplane/frame-1/test/api.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="4g1R6Gj4zE29SSNKROrm")
-project = rf.workspace("mia-pqqxd").project("frame-y9ksc")
-model = project.version(1).model
-
-print(model.predict(r"C:\Users\edayu\PycharmProjects\Dental\yolov8\frame-1\test\images\frame_21110_jpg.rf.170f09e4fec435be95940fdfed11f454.jpg", confidence= 40, overlap= 30).save("prediction.jpg"))
